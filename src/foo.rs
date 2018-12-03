581b7a28154f4e897f669ded259d80f64970a18c
Paolo Bonzini <pbonzini@redhat.com>
2018-12-03 12:46:47 +0100

    "rustify" implementation, with bindings provided as extern functions
    Finally in this patch we complete the exercise and we are able to enjoy
    the benefits of Rust in implementing the Foo data structure.
    
    The data structure is still defined in a C header, and the
    "bindgen::foo:Foo" struct that bindgen created for us is still what
    gets re-exported as "foo::Foo".  However, it now grows Rust methods,
    including a constructor "Foo::new" that was not even there in the original
    C API, and the C API becomes a simple wrapper that converts the raw
    pointers to C code and calls those methods.  In this commit we add a
    second test, which calls those Rust methods instead of relying on the
    C API.  Note that this test does not have to use any unsafe block,
    and in fact none of the code added in this commit does.

--- src/foo.rs
+++ src/foo.rs
@@ -1,15 +1,29 @@
 pub use bindgen::foo::Foo;
 
+impl Foo {
+    pub fn new() -> Foo {
+        return Foo { x: 0 }
+    }
+
+    pub fn get(&self) -> i32 {
+        self.x
+    }
+
+    pub fn set(&mut self, value: i32) {
+        self.x = value;
+    }
+}
+
 #[no_mangle]
 pub unsafe extern fn foo_get(foo: *const Foo) -> std::os::raw::c_int {
     let foo = &*foo;
-    foo.x
+    foo.get()
 }
 
 #[no_mangle]
 pub unsafe extern fn foo_set(foo: *mut Foo, value: std::os::raw::c_int) {
     let foo = &mut *foo;
-    foo.x = value
+    foo.set(value);
 }
 
 #[cfg(test)]
@@ -25,5 +39,12 @@ mod tests {
             foo_print(&foo);
             assert_eq!(foo_get(&foo), 5);
         }
+    }
+
+    #[test]
+    fn rust_api() {
+        let mut foo = Foo::new();
+        foo.set(5);
+        assert_eq!(foo.get(), 5);
     }
 }
